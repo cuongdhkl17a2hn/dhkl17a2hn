cd5c80f482298bf663598982ad9f338f24f0e1cb
khoa hoc du lieu 17a2 <nmcuong.dhkl17A2hn@sv.uneti.edu.vn>
2023-11-11 03:36:31 +0700

    Them file

---
+++ 1.3_Chuong5.py
@@ -0,0 +1,6 @@
+#DAY LA CHUONG TRINH SO BON
+#Viet chuong trinh tinh toan don gian
+print("ten_hang" ,"=","Sua hop Vinamilk")
+print("so_luong" ,"=" ,"5")
+print("don_gia" , "=" ,"25000")
+print('tien_phai_tra', 5, '*' , 25000, '=',125000)
