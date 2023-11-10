f87b4fe3cad11c3e7a2486d3f36027690677c5e2
khoa hoc du lieu 17a2 <nmcuong.dhkl17A2hn@sv.uneti.edu.vn>
2023-11-11 03:38:50 +0700

    Them file

---
+++ 1.1_Chuong5.py
@@ -0,0 +1,7 @@
+#DAY LA CHUONG TRINH THU SAU
+#in ra man hinh dong chu HELLO
+print("**    **  ******  **      **      ******")
+print("**    **  **      **      **      **  **")
+print("********  ******  **      **      **  **")
+print("**    **  **      **      **      **  **")
+print("**    **  ******  ******  ******  ******")
