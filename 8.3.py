d1300dd13fb617f83fe3ce82b5400ae21585afe2
khoa hoc du lieu 17a2 <nmcuong.dhkl17A2hn@sv.uneti.edu.vn>
2023-11-11 03:19:13 +0700

    Them file

---
+++ 8.3.py
@@ -0,0 +1,11 @@
+a=float(input("Nhap so a : "))
+b=float(input("Nhap so b : "))
+if a==0:
+    if b==0:
+        print("Vo so nghiem")
+    else:
+        print("Vo nghiem")
+else:
+    print("Phuong trinh co nghiem x = ", -b/a)
+    
+        
