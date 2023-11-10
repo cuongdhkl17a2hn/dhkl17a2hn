454a9fa2725403f02aa6a4e3b3f6a9d2840be1a2
khoa hoc du lieu 17a2 <nmcuong.dhkl17A2hn@sv.uneti.edu.vn>
2023-11-11 03:34:27 +0700

    Them file

---
+++ 1.2_Chuong5.py
@@ -0,0 +1,15 @@
+#DAY LA CHUONG TRINH SO NAM
+#Thuc hien phep tinh toan don gian
+x=10
+y=5
+tong=x+y
+hieu=x-y
+tich=x*y
+thuong=x/y
+print("tong cua hai so" , x, "+" , y, "=" ,tong)
+print("hieu cua hai so" , x, "-" , y, "=" ,hieu)
+print("tich cua hai so" , x, "*" , y, "=" ,tich)
+print("thuong cua hai so" , x, "/" , y, "=" ,thuong)
+import math
+can_bac_hai = math.sqrt(x)
+print("can bac hai cua" , x, "=" , can_bac_hai)
