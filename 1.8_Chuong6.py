fb0f45fcb736c4b46b2db5715259025aec01956f
khoa hoc du lieu 17a2 <nmcuong.dhkl17A2hn@sv.uneti.edu.vn>
2023-11-11 03:59:52 +0700

    Them file

---
+++ 1.8_Chuong6.py
@@ -0,0 +1,12 @@
+#baitap1.8
+S1=float(input("Nhập S1 : "))
+S2=float(input("Nhập S2 : "))
+S3=float(input("Nhập S2 : "))
+index=int(input("Nhập chỉ mục index : "))
+chiều_dài_S1 = len(S1)
+chiều_dài_S2 = len(S2)
+chiều_dài_S3 = len(S3)
+print(f"Chiều dài của chuỗi S1: {chieu_dai_S1}")
+print(f"Chiều dài của chuỗi S2: {chieu_dai_S2}")
+print(f"Chiều dài của chuỗi S3: {chieu_dai_S3}")
+S4=S1[index:]
