8c058ebe0ca97d24d9c47a9ebfde1b16590e2626
khoa hoc du lieu 17a2 <nmcuong.dhkl17A2hn@sv.uneti.edu.vn>
2023-11-11 03:52:13 +0700

    Them file

---
+++ 1.5_Chuong6.py
@@ -0,0 +1,5 @@
+#baitap1.5
+Soluong=float(input("nhap So luong : "))
+Dongia=float(input("nhap Don gia : "))
+Thanhtien=Soluong*Dongia
+print(Soluong, "+", Dongia, "=", Thanhtien)
---
+++ 1.6_Chuong6.py
@@ -0,0 +1,6 @@
+#baitap1.6
+Alice_candies = 121
+Bob_candies = 77
+Carol_candies = 109
+to_smash = (Alice_candies + Bob_candies + Carol_candies)%3
+print("Số kẹo dư bị đập là =", to_smash)
